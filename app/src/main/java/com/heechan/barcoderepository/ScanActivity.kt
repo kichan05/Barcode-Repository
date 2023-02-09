91f2c93cb6c83cda506b49b15212ef4ce22c7518
Heechan <ckstmznf@naver.com>
2023-02-10 00:21:19 +0900

    Preview barcode

--- app/src/main/java/com/heechan/barcoderepository/ScanActivity.kt
+++ app/src/main/java/com/heechan/barcoderepository/ScanActivity.kt
@@ -5,17 +5,23 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.google.zxing.BarcodeFormat
 import com.heechan.barcoderepository.databinding.ActivityScanBinding
+import com.journeyapps.barcodescanner.BarcodeEncoder
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 
 class ScanActivity : AppCompatActivity() {
-    lateinit var binding : ActivityScanBinding
+    lateinit var binding: ActivityScanBinding
     private val barcodeLauncher = registerForActivityResult(ScanContract()) { result ->
         if (result.contents == null) {
             Toast.makeText(this, "바코드 스캔에 실패했습니다.", Toast.LENGTH_LONG).show()
         } else {
-            Toast.makeText(this, "Scanned : ${result.contents}", Toast.LENGTH_LONG).show()
+            val barcodeEncoder = BarcodeEncoder()
+            val bitmap = barcodeEncoder.encodeBitmap(result.contents, BarcodeFormat.CODE_128, 400, 200)
+
+            binding.txtScanBarcodeID.text = result.contents
+            binding.imgScanBarcode.setImageBitmap(bitmap)
         }
     }
 
@@ -35,16 +41,16 @@ class ScanActivity : AppCompatActivity() {
         barcodeLauncher.launch(scanOption)
     }
 
-    private fun checkInput() : Boolean {
+    private fun checkInput(): Boolean {
         val name = binding.edtScanBarcodeName.text.toString()
         val description = binding.edtScanBarcodeDescription.text.toString()
 
-        if(name.isBlank()){
+        if (name.isBlank()) {
             binding.edtScanBarcodeName.error = "이름을 입력해주세요."
             return false
         }
 
-        if (description.isBlank()){
+        if (description.isBlank()) {
             binding.edtScanBarcodeDescription.error = "설명을 입력해주세요."
             return false
         }
@@ -52,8 +58,8 @@ class ScanActivity : AppCompatActivity() {
         return true
     }
 
-    private val saveBarcode = saveBarcode@{ view : View ->
-        if(!checkInput()) {
+    private val saveBarcode = saveBarcode@{ view: View ->
+        if (!checkInput()) {
             return@saveBarcode
         }
 
